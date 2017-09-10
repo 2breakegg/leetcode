9ef29f524494d8c5167c71f1d62954a9e724f638
2breakegg <1216023881@qq.com>
2017-09-10 17:43:04 +0800

    19

---
+++ 19. Remove Nth Node From End of List.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let arr=[];
+    let node=head;
+    while(node!=null){
+        arr.push(node);
+        node=node.next;
+    }
+    if(arr.length-n>0){
+        arr[arr.length-n-1].next=arr[arr.length-n].next;
+        return head;
+    }else if(arr.length>=2){
+        return arr[1];
+    }else{
+        return [];
+    }
+};
